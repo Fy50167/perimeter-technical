7504a855af9a28349c9f76c4608d7cec1da85627
Francis Yang <fy50167@gmail.com>
2024-03-30 16:37:20 -0700

    refactor code from react-mapbox-gl to mapbox-gl

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,69 +1,51 @@
 'use client';
 
-import ReactMapboxGl, {
-    Layer,
-    Feature,
-    Source,
-    Marker,
-    ZoomControl,
-} from 'react-mapbox-gl';
-import { useState, useEffect } from 'react';
-import './mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { useState, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 export default function Map() {
-    // Hold user position to set default map location
-    const [userPosition, setUserPosition] = useState<[number, number] | null>(
-        null
-    );
-    const [someState, setSomeState] = useState<number>(0);
+    const mapContainer = useRef<any>(null);
+    const map = useRef<any>(null);
 
-    const Map = ReactMapboxGl({
-        accessToken:
-            'pk.eyJ1IjoiZnJhbmNpcy15YW5nIiwiYSI6ImNsdWJ4d3h5MzExNTMya2s0a2x0M3MybzAifQ.pkLkcbf73zZS0gEUzHz47A',
-    });
+    // Contain current longitude and latitude
+    const [lng, setLng] = useState<number | null>(null);
+    const [lat, setLat] = useState<number | null>(null);
+    const [zoom, setZoom] = useState<number>(12);
+
+    mapboxgl.accessToken =
+        'pk.eyJ1IjoiZnJhbmNpcy15YW5nIiwiYSI6ImNsdWJ4d3h5MzExNTMya2s0a2x0M3MybzAifQ.pkLkcbf73zZS0gEUzHz47A';
 
     const position = () => {
-        console.log(userPosition);
-    };
-
-    const changeState = () => {
-        setSomeState(someState + 1);
+        console.log([lng, lat]);
     };
 
     useEffect(() => {
-        // Fetch user's current position using browser's geolocation API
+        // Fetch user's current position using browser's geolocation API and set default longitutde and latitude
         navigator.geolocation.getCurrentPosition((position) => {
             const { longitude, latitude } = position.coords;
-            setUserPosition([longitude, latitude]);
+            setLng(longitude);
+            setLat(latitude);
         });
     }, []);
 
+    useEffect(() => {
+        if (lng !== null && lat !== null && !map.current) {
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [lng, lat],
+                zoom: zoom,
+            });
+        }
+    }, [lng, lat]);
+
     return (
         <>
-            {userPosition ? (
-                <div
-                    className='w-4/5 h-[45rem] p-4 bg-slate-200 rounded-md'
-                    onClick={() => changeState()}
-                >
+            {lng && lat ? (
+                <div className='w-4/5 h-[45rem] p-4 bg-slate-200 rounded-md'>
                     <div className='h-full w-full border-2 border-black border-solid rounded-md overflow-hidden'>
-                        <Map
-                            style='mapbox://styles/mapbox/streets-v11'
-                            containerStyle={{
-                                height: '100%',
-                                width: '100%',
-                            }}
-                            zoom={[15]}
-                            center={userPosition}
-                        >
-                            <ZoomControl />
-                            <Layer
-                                type='symbol'
-                                id='marker'
-                                layout={{ 'icon-image': 'marker-15' }}
-                            >
-                                <Feature coordinates={userPosition} />
-                            </Layer>
-                        </Map>
+                        <div ref={mapContainer} className='w-full h-full'></div>
                     </div>
                 </div>
             ) : (
