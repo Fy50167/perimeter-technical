2daaff31aeabf87022e59a51f7799dcc35ec0be2
Francis Yang <fy50167@gmail.com>
2024-03-29 23:21:12 -0700

    switching to loading the css via a css file

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,46 +7,70 @@ import ReactMapboxGl, {
     Marker,
     ZoomControl,
 } from 'react-mapbox-gl';
-import { useState, useEffect, useRef } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
-import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import { useState, useEffect } from 'react';
+import './mapbox-gl.css';
 
 export default function Map() {
-    const mapContainerRef = useRef(null);
-    const mapRef = useRef(null);
-    const drawRef = useRef(null);
+    // Hold user position to set default map location
+    const [userPosition, setUserPosition] = useState<[number, number] | null>(
+        null
+    );
+    const [someState, setSomeState] = useState<number>(0);
+
+    const Map = ReactMapboxGl({
+        accessToken:
+            'pk.eyJ1IjoiZnJhbmNpcy15YW5nIiwiYSI6ImNsdWJ4d3h5MzExNTMya2s0a2x0M3MybzAifQ.pkLkcbf73zZS0gEUzHz47A',
+    });
+
+    const position = () => {
+        console.log(userPosition);
+    };
+
+    const changeState = () => {
+        setSomeState(someState + 1);
+    };
 
     useEffect(() => {
-        mapboxgl.accessToken =
-            'pk.eyJ1IjoiZnJhbmNpcy15YW5nIiwiYSI6ImNsdWJ4d3h5MzExNTMya2s0a2x0M3MybzAifQ.pkLkcbf73zZS0gEUzHz47A';
-
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [0, 0],
-            zoom: 1,
+        // Fetch user's current position using browser's geolocation API
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { longitude, latitude } = position.coords;
+            setUserPosition([longitude, latitude]);
         });
-
-        const draw = new MapboxDraw({
-            displayControlsDefault: false,
-            controls: {
-                polygon: true,
-                trash: true,
-            },
-        });
-
-        mapRef.current = map;
-        drawRef.current = draw;
-
-        map.addControl(draw);
-
-        return () => {
-            map.remove();
-        };
     }, []);
 
     return (
-        <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />
+        <>
+            {userPosition ? (
+                <div
+                    className='w-4/5 h-[45rem] p-4 bg-slate-200 rounded-md'
+                    onClick={() => changeState()}
+                >
+                    <div className='h-full w-full border-2 border-black border-solid rounded-md overflow-hidden'>
+                        <Map
+                            style='mapbox://styles/mapbox/streets-v11'
+                            containerStyle={{
+                                height: '100%',
+                                width: '100%',
+                            }}
+                            zoom={[15]}
+                            center={userPosition}
+                        >
+                            <ZoomControl />
+                            <Layer
+                                type='symbol'
+                                id='marker'
+                                layout={{ 'icon-image': 'marker-15' }}
+                            >
+                                <Feature coordinates={userPosition} />
+                            </Layer>
+                        </Map>
+                    </div>
+                </div>
+            ) : (
+                <p className='mt-5 text-heading2-semibold'>
+                    Obtaining current location...
+                </p>
+            )}
+        </>
     );
 }
