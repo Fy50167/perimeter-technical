4adfaa71bb6229205b5d13a5b2ed2fdbbd4fb4db
Francis Yang <fy50167@gmail.com>
2024-04-04 19:03:43 -0700

    container for instructions

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,35 @@
+'use client';
+
 import Image from 'next/image';
 import Map from '../../components/Map';
+import { useState } from 'react';
+import Instructions from '@/components/Instructions';
 
 export default function Home() {
+    const [isHovered, setIsHovered] = useState<boolean>(false);
     return (
         <main className='min-h-screen min-w-screen bg-gradient flex flex-col justify-center items-center gap-4 p-4 overflow-auto'>
-            <div className='flex flex-col justify-center items-center'>
-                <h1 className='md:text-heading1-bold text-heading3-bold'>
+            <div className='flex flex-col justify-center items-center bg-maroon-2 px-8 py-4 rounded-md border-2 border-gold-2 border-solid relative'>
+                <div className='w-auto h-auto absolute top-2 right-2 m-0 p-0'>
+                    <div className='relative w-full h-full flex flex-col'>
+                        <Image
+                            src='/assets/question.png'
+                            alt='question mark icon'
+                            width={14}
+                            height={14}
+                            className='md:w-[1.25rem] md:h-[1.25rem] hover:cursor-pointer'
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                        />
+                        {isHovered && <Instructions />}
+                    </div>
+                </div>
+                <h1 className='md:text-heading1-bold text-body-bold w-full'>
                     Perimeter Technical Exercise
                 </h1>
-                <h3 className='text-base-medium'>By: Francis Yang</h3>
+                <h3 className='text-small-medium md:text-base-medium'>
+                    By: Francis Yang
+                </h3>
             </div>
             <Map />
         </main>
---
+++ src/components/Instructions.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Instructions = () => {
+    return (
+        <div className='absolute right-[-4px] md:right-0 top-6 md:top-8 w-[12rem] md:w-[12rem] z-20'>
+            <div className='relative'>
+                <div className='bg-slate-500 rounded-md border-black border-2 border-solid p-2 flex flex-col'>
+                    <h2 className='text-small-semibold'>How to use:</h2>
+                    <ul className='text-small-regular list-[circle]'>
+                        <li style={{ listStyleType: 'none' }}>
+                            To make markers on the map, simply click/tap. All
+                            markers are draggable.
+                        </li>
+                    </ul>
+                </div>
+                <div className='absolute right-0 top-0 w-4 h-4 transform rotate-45 -mt-1 mr-1'>
+                    <div className='bg-slate-500 w-full h-full border-black border-t-2 border-solid z-30' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Instructions;
